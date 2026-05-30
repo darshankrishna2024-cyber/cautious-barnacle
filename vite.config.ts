fc852110b572073655a6ac4f62d0561b8af3e830
darshankrishna2024-cyber <darshankrishna2024@gmail.com>
2026-05-30 18:17:46 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+export default defineConfig({
+  plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    tanstackStart({
+      server: {
+        entry: "server",
+      },
+    }),
+    react(),
+  ],
+});
